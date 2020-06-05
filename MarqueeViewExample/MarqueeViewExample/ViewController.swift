b3cc08d38723f49af902c0c7cb7329fc22578d34
Paul.Chou <paul.chou.dev@gmail.com>
2020-06-05 10:51:19 +0800

    feat: update example

--- MarqueeViewExample/MarqueeViewExample/ViewController.swift
+++ MarqueeViewExample/MarqueeViewExample/ViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var marquee : Marquee?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let strings = ["Hello world!Hello world!Hello world!", "Testing Marquee!!", "This is sparta", "Fuck IQiYi", "FUCK FUCK FUCK FUCK FUCK FUCK"]
-        let marquee = Marquee.init(with: strings)
-        marquee.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(marquee)
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[marquee]|", options: [], metrics: nil, views: ["marquee": marquee]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-100-[marquee(44)]", options: [], metrics: nil, views: ["marquee": marquee]))
-        marquee.setupUI()
-        view.layoutSubviews()
-        marquee.setupAni()
+        let strings = ["Hello world! Hello world! Hello world!", "Testing Marquee!!", "This is sparta", "Let's try do something new!!", "This is TAIWAN number ONE!!"]
+
+        marquee = Marquee.init(with: strings)
+        if let marquee = marquee {
+            marquee.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(marquee)
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[marquee]|", options: [], metrics: nil, views: ["marquee": marquee]))
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-100-[marquee(44)]", options: [], metrics: nil, views: ["marquee": marquee]))
+            marquee.setupUI()
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        marquee?.setupAni()
     }
 
 
